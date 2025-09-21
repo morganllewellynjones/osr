8ab411bced5eb906d31bb2787c02624de3ad850e
morgan <morganjones.gm@gmail.com>
2025-09-21 12:35:10 -0700

    Initialized Spring Boot

---
+++ src/main/java/com/osr/OsrApplication.java
@@ -0,0 +1,13 @@
+package com.osr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrApplication.class, args);
+	}
+
+}
