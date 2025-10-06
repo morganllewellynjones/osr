8e7ebb304904393596347c9d2a7ed3636c985415
morgan <morganjones.gm@gmail.com>
2025-10-05 17:13:15 -0700

    Setup Scripts to initialize accounts and player characters

---
+++ scripts/Account.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS account;
+
+CREATE TABLE IF NOT EXISTS account.roles(
+    role TEXT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS account.profile(
+    profile_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(255) REFERENCES account.roles (role),
+    created_timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_timestamp TIMESTAMPTZ
+);
+
+
+INSERT INTO account.roles (role) VALUES
+    ('player'),
+    ('storyteller'),
+    ('admin');
