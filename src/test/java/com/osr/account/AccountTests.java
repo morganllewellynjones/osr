645e253f8926d8723b4ae6d57378b342b71b583f
morgan <morganjones.gm@gmail.com>
2026-02-16 08:54:03 -0800

    Renamed accounts and characters for ease of testing. Added test for duplicate list entries.

--- src/test/java/com/osr/account/AccountTests.java
+++ src/test/java/com/osr/account/AccountTests.java
@@ -51,12 +51,12 @@ class AccountTests {
 	
 	@Transactional
 	void setupData() {
-	    Account a = new Account("donkey_kong", "snowflake", "player");
-	    Character arthur = new Character("arthur", a);
-	    Character merlin = new Character("merlin", a);
-	    Character viviane = new Character("viviane", a);
+	    Account a = new Account("player1", "pass1", "player");
+	    Character character1 = new Character("character1", a);
+	    Character character2 = new Character("character2", a);
+	    Character character3 = new Character("character3", a);
 	    accountRepository.save(a);
-	    characterRepository.saveAll(List.of(arthur, merlin, viviane));
+	    characterRepository.saveAll(List.of(character1, character2, character3));
 	    entityManager.flush();
 	    entityManager.clear();
 	}
@@ -65,9 +65,9 @@ class AccountTests {
 	@Transactional
 	@DisplayName("")
 	void accountWithCharacters() {
-	    logger.info("Fetching Donkey Kong");
-	    var account = accountRepository.findByUsername("donkey_kong");
-	    logger.info("Finished with Donkey Kong!");
+	    logger.info("Fetching player1");
+	    var account = accountRepository.findByUsername("player1");
+	    logger.info("Finished with player1!");
 	}
 
 	@Test
@@ -76,8 +76,8 @@ class AccountTests {
 	void testAssociationFetching() {
 	    // Note: If you change the association from account -> characters as an eager fetch type
 	    // Then you will see queries both to fetch the account and the characters when we get the characters
-	    logger.info("Fetching donkey_kong player account");
-	    var a = accountRepository.findByUsername("donkey_kong");
+	    logger.info("Fetching player1 player account");
+	    var a = accountRepository.findByUsername("player1");
 	    logger.info(a.toString());
 
 	    logger.info("Fetching account characters");
@@ -89,8 +89,8 @@ class AccountTests {
 	@Transactional
 	@DisplayName("Test to see that a newly created account is equal to an existing persistent account with the same name.")
 	void testEquality() {
-	    Account a = new Account("donkey_kong", "new_password", "new_role");
-	    Account b = accountRepository.findByUsername("donkey_kong");
+	    Account a = new Account("player1", "new_password", "new_role");
+	    Account b = accountRepository.findByUsername("player1");
 	    logger.info(String.format("Does a = b? %b", a.equals(b)));
 	    assert(a.equals(b) == true);
 	}
@@ -99,35 +99,35 @@ class AccountTests {
 	@Transactional
 	@DisplayName("Test that hibernate properly detects a mutation to a persistent entity and flushes state without an explicity update.")
 	void testDirtyUpdate() {
-	    logger.info("Fetching first instance of donkey_kong");
-	    var a = accountRepository.findByUsername("donkey_kong");
-	    assert(a.password == "snowflake");
-	    logger.info("Fetching second instance of donkey_kong");
-	    var b = accountRepository.findByUsername("donkey_kong");
+	    logger.info("Fetching first instance of player1");
+	    var a = accountRepository.findByUsername("player1");
+	    assert(a.password == "pass1");
+	    logger.info("Fetching second instance of player1");
+	    var b = accountRepository.findByUsername("player1");
 	    logger.info("Changing password");
 	    b.password = "new_password";
 	    logger.info("Flushing state without explicitly updating the entity in the database");
 	    entityManager.flush();
 	    logger.info("Fetching same entity after a flush");
-	    var c = accountRepository.findByUsername("donkey_kong");
-	    logger.info("Checking that new reference to donkey_kong has the updated password");
+	    var c = accountRepository.findByUsername("player1");
+	    logger.info("Checking that new reference to player1 has the updated password");
 	    assert(c.password == "new_password");
 	}
 
 	@Test
 	@Transactional
 	void testCompetingDirtyUpdates() {
-	    logger.info("Fetching first instance of donkey_kong");
-	    var a = accountRepository.findByUsername("donkey_kong");
-	    assert(a.password == "snowflake");
+	    logger.info("Fetching first instance of player1");
+	    var a = accountRepository.findByUsername("player1");
+	    assert(a.password == "pass1");
 	    assert(a.role == "player");
 
 	    logger.info("Changing password and role from first reference");
 	    a.password = "new_password";
 	    a.role = "new_role";
-	    logger.info("Fetching second instance of donkey_kong");
+	    logger.info("Fetching second instance of player1");
 
-	    var b = accountRepository.findByUsername("donkey_kong");
+	    var b = accountRepository.findByUsername("player1");
 
 	    logger.info("Overwriting new password from second reference");
 
@@ -137,8 +137,8 @@ class AccountTests {
 	    entityManager.flush();
 	    
 	    logger.info("Fetching same entity after a flush");
-	    var c = accountRepository.findByUsername("donkey_kong");
-	    logger.info("Checking that new reference to donkey_kong has the updated password");
+	    var c = accountRepository.findByUsername("player1");
+	    logger.info("Checking that new reference to player1 has the updated password");
 	    assert(c.password == "other_new_password");
 	    assert(c.role == "new_role");
 	}
@@ -147,12 +147,29 @@ class AccountTests {
 	@Transactional
 	@DisplayName("Test to see if loading an entity and modifying its state causes a flush without an explicit save.")
 	void testDirtyUpdateWithoutSaving() {
-	    Account a = accountRepository.findByUsername("donkey_kong");
+	    Account a = accountRepository.findByUsername("player1");
 	    a.password = "new_password";
 	    a.role = "new_role";
 
 	    //It seems not. If you explicitly flush or call an action that requires a flush it will (for example a native query).
 	    logger.info("Will it flush?");
+	}
+
+	@Test
+	@Transactional
+	void testSavingDuplicateListEntries() {
+	    var a = accountRepository.findByUsername("player1");
+	    assert(a.characters.size() == 3);
+	    logger.info("Adding 3 new characters that already exist, and one new character");
+	    a.characters.addAll(List.of(
+		new Character("character1", a),
+		new Character("character2", a),
+		new Character("character3", a),
+		new Character("character_actually_new", a)
+	    ));
+	    logger.info("There should now be 4 characters (not 7), because 3 of the new characters were already persistent.");
+	    logger.info(String.format("Characters size: %d", a.characters.size()));
+	    assert(a.characters.size() == 4);
 	}
 
 	@Test
@@ -169,7 +186,7 @@ class AccountTests {
 		    new Account("new_1", "pass", "player"),
 		    new Account("new_2", "pass", "player"),
 		    new Account("new_3", "pass", "player"),
-		    new Account("donkey_kong", "new_pass", "new_role")
+		    new Account("player1", "new_pass", "new_role")
 	    );
 
 	    logger.info("Creating the new accounts");
@@ -191,7 +208,7 @@ class AccountTests {
 
 	    logger.info("Fetching accounts");
 	    var allAccounts = accountRepository.findAll();
-	    var updatedAccount = accountRepository.findByUsername("donkey_kong");
+	    var updatedAccount = accountRepository.findByUsername("player1");
 	    assert(updatedAccount.password == "new_pass");
 	    assert(updatedAccount.role == "new_role");
 	    
